d13d31b3edee550cc45713bbc5e6b971890622f7
gabriel-araujo912 <gabriel.araujo98@hotmail.com>
2024-07-24 00:06:15 -0300

    Commit 2

---
+++ Gabflix/Filmes.js
@@ -0,0 +1,71 @@
+function VEF8() /* <!-- Veloses e Furiosos 8 */ {
+  const YouTube = document.querySelector('.VEF8');
+  const Iframe = document.querySelector('iframe');
+  YouTube.classList.toggle('active')
+  Iframe.currentTime = 0;
+  Iframe.pause();
+}
+
+function JWAJ() /* Acampamento Jurrássic */ {
+  const YouTube = document.querySelector('.JWAJ');
+  const Iframe = document.querySelector('iframe');
+  YouTube.classList.toggle('active')
+  Iframe.currentTime = 0;
+  Iframe.pause();
+}
+
+function ALFA() /* Alfa - O Filme */ {
+  const YouTube = document.querySelector('.ALFA');
+  const Iframe = document.querySelector('iframe');
+  YouTube.classList.toggle('active')
+  Iframe.currentTime = 0;
+  Iframe.pause();
+}
+
+function MOGLI() /* Mogli: O Menino Lobo */ {
+  const YouTube = document.querySelector('.MOGLI');
+  const Iframe = document.querySelector('iframe');
+  YouTube.classList.toggle('active')
+  Iframe.currentTime = 0;
+  Iframe.pause();
+}
+
+function ARCANE() /* Arcane */ {
+  const YouTube = document.querySelector('.ARCANE');
+  const Iframe = document.querySelector('iframe');
+  YouTube.classList.toggle('active')
+  Iframe.currentTime = 0;
+  Iframe.pause();
+}
+
+function JWMD() /* Jurrássic World - O Mundo dos Dinossauros */ {
+  const YouTube = document.querySelector('.JWMD');
+  const Iframe = document.querySelector('iframe');
+  YouTube.classList.toggle('active')
+  Iframe.currentTime = 0;
+  Iframe.pause();
+}
+
+function JWD() /* Jurrássic World - Dominio */ {
+  const YouTube = document.querySelector('.JWD');
+  const Iframe = document.querySelector('iframe');
+  YouTube.classList.toggle('active')
+  Iframe.currentTime = 0;
+  Iframe.pause();
+}
+
+function JWRA() /* Jurrássic World - Reino Ameaçado */ {
+  const YouTube = document.querySelector('.JWRA');
+  const Iframe = document.querySelector('iframe');
+  YouTube.classList.toggle('active')
+  Iframe.currentTime = 0;
+  Iframe.pause();
+}
+
+function REOUD() /* Raya e o ultimo dragão */ {
+  const YouTube = document.querySelector('.REOUD');
+  const Iframe = document.querySelector('iframe');
+  YouTube.classList.toggle('active')
+  Iframe.currentTime = 0;
+  Iframe.pause();
+}
